d88cfc034e963ee67527039c7309f921ab2e0bfd
theobeo2004 <thenhuet2004@gmail.com>
2025-03-29 22:47:09 +0700

    feat: stream data to bronze storage instead of streaming directly to Kafka

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,7 +1,16 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
+import json, uuid, time, logging, io, requests
+
+from minio_config import config
+
+#Import MinIO client
+try:
+    from minio import Minio
+    from minio.error import S3Error
+except ImportError:
+    pass
 
 default_args = {
     'owner': 'airscholar',
@@ -12,7 +21,6 @@ default_args = {
 }
 
 def get_data():
-    import requests
     
     res = requests.get('https://randomuser.me/api/').json()
     res = res['results'][0]
@@ -31,15 +39,27 @@ def format_data(res):
     return data
     
 def stream_data():
-    import json
-    from kafka import KafkaProducer
-    import time
-    import logging
     
-    producer = KafkaProducer(bootstrap_servers=['broker:29092'],
-                            max_block_ms=5000)
+    # Initialize MinIO client to stream data into Bronze storage
+    minio_client = Minio(
+        "minio:9000",
+        access_key=config['access_key'],
+        secret_key=config['secret_key'],
+        secure=False
+    )
+    
+    # Check the existence of bronze storage under the name "bronze"
+    bucket_name = "bronze"
+    if not minio_client.bucket_exists(bucket_name):
+        logging.error(f"Bucket {bucket_name} does not exist")
+        try:
+            minio_client.make_bucket(bucket_name)
+            logging.info(f"Created bucket {bucket_name}")
+        except S3Error as e:
+            logging.error(f"Error creating bucket {e}")
+            return
+    
     curr_time = time.time()
-
     
     while True:
         if time.time() > curr_time + 60: # More than 1 minute
@@ -49,25 +69,36 @@ def stream_data():
             
             res = format_data(res)
             
-            producer.send('users.created', json.dumps(res).encode('utf-8'))
+            # Create file name
+            timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+            file_name = f"owo/user-{timestamp}-{uuid.uuid4().hex[:8]}.json"
             
-            time.sleep(5)
+            json_data = json.dumps(res).encode('utf-8')
+            
+            minio_client.put_object(
+                bucket_name,
+                file_name,
+                io.BytesIO(json_data),
+                len(json_data),
+                content_type='application/json'
+            )
+            
+            logging.info(f"Saved file {file_name} to MinIO bucket {bucket_name}")
+            
+            time.sleep(2)
             
         except Exception as e:
             logging.error(f'An error occured: {e}')
             continue
     
-    producer.flush()
-    producer.close()
-    
-with DAG('user_automation', 
+with DAG('experiment_stream', 
          default_args=default_args,
          schedule_interval='@daily',
          catchup=False) as dag:
 
     # Streaming data to Kafka
     streaming_task = PythonOperator(
-        task_id='stream_data_from_api',
+        task_id='load_data_to_minio',
         python_callable=stream_data
     )
     
@@ -82,6 +113,9 @@ with DAG('user_automation',
     # )
     
     streaming_task
+
+
+
 
 
 
