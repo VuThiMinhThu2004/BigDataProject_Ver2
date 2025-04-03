0bec59c34d237f9a948435df1980f2fe200b84b4
VuThiMinhThu2004 <vuthiminhthu04@gmail.com>
2025-04-03 18:04:13 +0700

    create mlflow

--- datapipe/minio_config.py
+++ datapipe/minio_config.py
@@ -1,6 +1,6 @@
 # Configure & set access_key and secret_key manually. 
 # The setting values ​​are different for different locales.
 config = {
-    'access_key': 'QeJHHQrOjScQa2cfiCqM',
-    'secret_key': 'AAengmKnTEomI7RYoaywdpPWaKvekKnSb58Vx3sI'
+    'access_key': 'fBA2Tos2WNA5E6zllx4n',
+    'secret_key': 'IvdTdzNe6jdOtSp53QGcL8avTNvvc7XZYBWhRnuP'
 }
