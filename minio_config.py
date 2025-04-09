a09f9473f0b0ee8c9bdea68b49071a91d0bcddb8
theobeo2004 <thenhuet2004@gmail.com>
2025-04-09 11:23:55 +0700

    refactor branch - ref: removing

--- minio_config.py
+++
@@ -1,6 +0,0 @@
-# Configure & set access_key and secret_key manually. 
-# The setting values ​​are different for different locales.
-config = {
-    'access_key': 'QeJHHQrOjScQa2cfiCqM',
-    'secret_key': 'AAengmKnTEomI7RYoaywdpPWaKvekKnSb58Vx3sI'
-}
