02530b4a703dc8bdf61f9cfd86dfbe9334ce7786
theobeo2004 <thenhuet2004@gmail.com>
2025-04-13 18:14:51 +0700

    main branch - delete duplicated components

--- datapipe/minio_config.py
+++
@@ -1,6 +0,0 @@
-# Configure & set access_key and secret_key manually. 
-# The setting values ​​are different for different locales.
-config = {
-    'access_key': 'T6sMJ0ENWHsyVWWXdMlI',
-    'secret_key': 'w9dciXDM8doZKay6PAbB4y2HF3x2wKPQtqKSfKgE'
-}
