b27967bcaee7f4c2396dc579bab273d3d96795ac
VuThiMinhThu2004 <vuthiminhthu04@gmail.com>
2025-04-15 18:19:33 +0700

    from datahouse

--- datalake/minio_config.py
+++ datalake/minio_config.py
@@ -1,6 +1,6 @@
 # Configure & set access_key and secret_key manually. 
 # The setting values ​​are different for different locales.
 config = {
-    'access_key': 'K2DL9ybAAN88XDykKLH1',
-    'secret_key': 'x4aAzu7UpzV6yw1omMxpHvIHzc55xHC0Sfpa8N3K'
+    'access_key': 'SbstzBp81M0Jkhpio2ju',
+    'secret_key': 'yBle4P2A0SOR6lmS6wdKgBsfWWcDrVpPpygCKwW7'
 }
