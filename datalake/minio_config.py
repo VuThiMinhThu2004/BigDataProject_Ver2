9e5bb49c4dac434f02ba0b95b6e28c669d540c95
Chi290804 <chihoangf@gmail.com>
2025-04-19 00:59:26 +0700

    dataset_path

--- datalake/minio_config.py
+++ datalake/minio_config.py
@@ -1,6 +1,7 @@
 # Configure & set access_key and secret_key manually. 
 # The setting values ​​are different for different locales.
+# Comment lai giup Chi nha em
 config = {
-    'access_key': 'SbstzBp81M0Jkhpio2ju',
-    'secret_key': 'yBle4P2A0SOR6lmS6wdKgBsfWWcDrVpPpygCKwW7'
+    'access_key': 'T6sMJ0ENWHsyVWWXdMlI',
+    'secret_key': 'w9dciXDM8doZKay6PAbB4y2HF3x2wKPQtqKSfKgE'
 }
