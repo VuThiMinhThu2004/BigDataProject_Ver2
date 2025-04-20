46c058afd2b9309c1674e46fa39228985f25700d
theobeo2004 <thenhuet2004@gmail.com>
2025-04-20 16:14:24 +0700

    reduce_pipeline - ref: config datasource

--- data_source/load_to_source.py
+++ data_source/load_to_source.py
@@ -7,7 +7,7 @@ import json
 
 from minio_config import config
 
-def upload_to_storage(minio_client, bucket_name, buffer, file_name):
+def upload_to_source(minio_client, bucket_name, buffer, file_name):
     
     try:
         minio_client.put_object(
@@ -17,50 +17,23 @@ def upload_to_storage(minio_client, bucket_name, buffer, file_name):
             length = buffer.getbuffer().nbytes,
             content_type="application/octet-stream"
         )
-        # logging.info(f"Successfully uploaded {file_name} to bucket {bucket_name}")
     except Exception as e:
         logging.error(f"Something failed when attempt to upload f{file_name}")
     
 
-# def clean_data
-
-def csv_to_parquet(minio_client, bucket_name):
-    
-    # df = pd.read_csv('/root/thu/BigDataProject/datalake/validated_streaming.csv')
-    df = pd.read_csv('./datalake/validated_streaming.csv')
-    df['event_time'] = pd.to_datetime(df['event_time'])
-    df['date_hour'] = df['event_time'].dt.strftime('%Y-%m-%d %H')
-
-    grouped = df.groupby('date_hour')
-    
-    for date_hour, group in grouped:
-        # Create Parquet in memory
-        buffer = io.BytesIO()
-        group.drop(columns=['date_hour']).to_parquet(buffer, index=False)
-        
-        file_name = f"traing_data/data_{date_hour.replace(' ', '_')}.parquet"
-        
-        # Set cursor to beginner of buffer
-        buffer.seek(0)
-        
-        # Upload to "storage" bucket
-        upload_to_storage(minio_client=minio_client, bucket_name=bucket_name, buffer=buffer, file_name=file_name)
-
 def csv_to_individual_json(minio_client, bucket_name):
     """
-    Chuyển từng hàng của DataFrame thành file JSON riêng biệt và lưu vào MinIO
+    Convern each row from .csv into json object and upload to data_source based on MinIO
     """
     try:
         logging.info("Starting to convert CSV rows to individual JSON files")
         
         # Đọc file CSV
-        df = pd.read_csv('./datalake/validated_streaming.csv')
+        df = pd.read_csv('train_clean_small.csv')
         
         # Chuyển event_time thành datetime sau đó thành chuỗi để tránh lỗi serialization
-        df['event_time'] = pd.to_datetime(df['event_time'])
-        df['event_time'] = df['event_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
-        
-        df = df.dropna(axis=0)
+        df['event_timestamp'] = pd.to_datetime(df['event_timestamp'])
+        df['event_timestamp'] = df['event_timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
         
         # Tạo thêm cột ID duy nhất cho mỗi hàng nếu chưa có
         df['row_id'] = range(1, len(df) + 1)
@@ -70,8 +43,8 @@ def csv_to_individual_json(minio_client, bucket_name):
             # Chuyển đổi hàng thành dict
             row_dict = row.to_dict()
 
-            # Tạo cấu trúc thư mục phân cấp và tên file
-            file_name = f"testing_data/{row_dict['row_id']}.json"
+            # Tạo tên file
+            file_name = f"{row_dict['row_id']}.json"
             
             # Loại bỏ row_id từ dữ liệu cuối cùng nếu không muốn lưu
             if 'row_id' in row_dict:
@@ -84,7 +57,7 @@ def csv_to_individual_json(minio_client, bucket_name):
             buffer = io.BytesIO(json_string.encode('utf-8'))
             
             # Upload lên MinIO
-            upload_to_storage(
+            upload_to_source(
                 minio_client=minio_client, 
                 bucket_name=bucket_name, 
                 buffer=buffer, 
@@ -102,11 +75,11 @@ def csv_to_individual_json(minio_client, bucket_name):
 
 def main():
     try:
-        # Thiết lập logging
+        # Config logging
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-        logging.info("Starting data processing")
+        logging.info("Starting data transfering")
         
-        # Kết nối MinIO
+        # Minio connection
         minio_client = Minio(
             "localhost:9000",
             access_key=config['access_key'],
@@ -114,7 +87,7 @@ def main():
             secure=False
         )
         
-        bucket_name = "storage" 
+        bucket_name = "datasource" 
         
         if not minio_client.bucket_exists(bucket_name=bucket_name):
             logging.warning(f"Bucket {bucket_name} does not exist")
@@ -123,17 +96,12 @@ def main():
         
         # Chuyển từng hàng của CSV thành file JSON riêng biệt
         logging.info("Converting CSV rows to individual JSON files...")
-        csv_to_individual_json(minio_client=minio_client, bucket_name="storage")
-        csv_to_individual_json(minio_client=minio_client, bucket_name="bronze")
-
-        # Chuyển CSV thành Parquet
-        # logging.info("Converting CSV to Parquet...")
-        # csv_to_parquet(minio_client=minio_client, bucket_name=bucket_name)
+        csv_to_individual_json(minio_client=minio_client, bucket_name=bucket_name)
         
-        logging.info("Data processing completed successfully")
+        logging.info("Data transfering completed successfully")
         
     except Exception as e:
-        logging.error(f"Failed to process data: {e}")
+        logging.error(f"Failed to transfer data: {e}")
 
 if __name__ == "__main__":
     main()
---
+++ data_source/minio_config.py
@@ -0,0 +1,4 @@
+config = {
+    'access_key': 'B99OiGPYaMMoPhokSQeJ',
+    'secret_key': '98OiAXTdMk32tumHqOO2ChOCmISxwmxjyULhabnk'
+}
